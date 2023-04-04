68fe4e8a064bf560d9419f3f2607729b5299ad0c
Julian Lescano <lescano.julian22@gmail.com>
2023-04-04 14:29:13 -0300

    validations for categories created with joi

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { getAllCategories, deleteCategory,
 createCategory, updateCategory, 
 findCategoryById  } from '../controllers/category';
+import { createCategoryValidation, updateCategoryValidation } from '../validations/categories';
 
 
 const router = express.Router();
@@ -9,8 +10,8 @@ const router = express.Router();
 router
     .get('/', getAllCategories)
     .get('/:id', findCategoryById)
-    .post('/', createCategory)
-    .put('/:id', updateCategory)
+    .post('/', createCategoryValidation, createCategory)
+    .put('/:id',updateCategoryValidation, updateCategory)
     .delete('/:id', deleteCategory)
 
 export default router;
---
+++ src/validations/categories.js
@@ -0,0 +1,39 @@
+import Joi from 'joi';
+
+export const createCategoryValidation = (req,res, next) => {
+    const newCategory = Joi.object({
+        name : Joi.string()
+        .min(3)
+        .max(30)
+        .pattern(/^[A-Za-z0-9 ]+$/)
+        .required()
+    })
+    const valid = newCategory.validate(req.body);
+    if(valid.error) {
+        return res.status(400).json({
+            message: valid.error.details[0].message,
+            data: undefined,
+            error: true,
+        });
+    }
+    return next();
+};
+
+export const updateCategoryValidation = (req, res, next) => {
+    const updateCategory = Joi.object({
+        name : Joi.string()
+        .min(3)
+        .max(30)
+        .pattern(/^[A-Za-z0-9 ]+$/)
+        .required()
+    })
+    const valid = updateCategory.validate(req.body);
+    if(valid.error) {
+        return res.status(400).json({
+            message: valid.error.details[0].message,
+            data: undefined,
+            error: true,
+        });
+    }
+    return next();
+}
